491165af4a43e8d84f07348c3d2462843d75eee0
Joandry <sjoandry@vt.edu>
2026-02-19 16:54:57 -0500

    chore: changed api get paths to be more readable

--- app/main.py
+++ app/main.py
@@ -40,32 +40,32 @@ async def device_raw(device_id: str, limit: int = 10):
 async def device_readings(device_id: str, limit: int = 20):
     return DataStore.get_device_readings(device_id, limit=limit)    
 
-@app.get("/devices/{device_id}/recent_device_readings")
+@app.get("/devices/{device_id}/readings/recent")
 async def recent_device_readings(device_id: str):
     return DataStore.get_recent_device_readings(device_id)
 
 
-@app.get("/devices/{device_id}/{sensor_name}/device_individual_sensor_readings")
+@app.get("/devices/{device_id}/{sensor_name}/readings")
 async def individual_sensor_readings(device_id: str, sensor_name: str, limit: int  = 20):
     return DataStore.get_device_sensor_readings(device_id, sensor_name, limit=limit)
 
-@app.get("/devices/{device_id}/{sensor_name}/device_readings_since")
+@app.get("/devices/{device_id}/{sensor_name}/readings/since")
 async def device_readings_since(device_id: str, sensor_name: str, since: datetime):
     return DataStore.get_device_sensor_readings_since(device_id, sensor_name, since)
 
-@app.get("/devices/{device_id}/device_readings_between")
+@app.get("/devices/{device_id}/readings/between")
 async def device_readings_between(device_id: str, start: datetime, end: datetime):
     return DataStore.get_device_readings_between(device_id, start, end)
 
-@app.get("/devices/{device_id}/{sensor_name}/singular_sensor_readings_between")
+@app.get("/devices/{device_id}/{sensor_name}/readings/between")
 async def singular_device_readings_between(device_id: str, sensor_name: str, start: datetime, end: datetime):
     return DataStore.get_device_singlular_sensor_readings_between(device_id, sensor_name, start, end)
 
-@app.get("/devices/{device_id}/{sensor_name}/sensor_stats")
+@app.get("/devices/{device_id}/{sensor_name}/stats")
 async def sensor_stats(device_id: str, sensor_name: str, start: datetime, end: datetime, limit: int = 20):
     return DataStore.get_sensor_stats(device_id, sensor_name, start, end)
 
-@app.get("/devices/{device_id}")
+@app.get("/devices/{device_id}/last-seen")
 async def device_last_seen(device_id: str):
     return DataStore.get_device_last_seen(device_id)
 
