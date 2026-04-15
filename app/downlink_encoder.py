77b06feb9de23abe3664eaa5288ebe743f90b987
Joandry <sjoandry@vt.edu>
2026-04-15 18:46:50 -0400

    added documentation to downlink encoder

--- app/downlink_encoder.py
+++ app/downlink_encoder.py
@@ -1,18 +1,28 @@
 
 class DownlinkEncoder:
+    """
+    encodes downlink commands into binary payloads
+    packet format:
+    [CMD][TARGET][LEN][DATA]
+
+    cmd: command identifier (1 byte)
+    target: sensor or subsystem (1byte)
+    len: number of bytes in data (1byte)
+    data: command specific payload
+    """
 
     #COMMAND LIST
     CMD_SET_INTERVAL = 0x01 #set a sensor interval
     CMD_REQUEST_STATUS = 0x02 #send next uplink immediately 
-    CMD_GET_REGION = 0x20 
+    CMD_GET_REGION = 0x20 #request lora region 
     CMD_SET_REGION = 0x21 #set lora region
     CMD_RESET = 0x22 #reset board
 
     SENSOR_IDS = {
-        "temperature": 0,
+        "temperature": 0, #bme280
         "humidity": 0,
         "pressure": 0,
-        "visible": 1,
+        "visible": 1, #si115x
         "ir": 1,
         "wind_vane": 2,
         "wind_speed": 3,
@@ -20,6 +30,17 @@ class DownlinkEncoder:
 
     @staticmethod
     def encode_set_interval(sensor_id: int, interval_minutes: int) -> bytes:
+        """
+        encode interval update command
+        payload: [0x01][sensor_id][0x04][seconds (4bytes, big-endian)]
+
+        args: 
+        sensor_id(int) :sensor target id
+        interval_minutes (int): interval in minutes)
+
+        returns:
+        bytes
+        """
         seconds = interval_minutes * 60
         return bytes([
             DownlinkEncoder.CMD_SET_INTERVAL,
@@ -33,6 +54,12 @@ class DownlinkEncoder:
     
     @staticmethod
     def encode_request_status() -> bytes:
+        """
+        encode request status command
+        payload: 
+        [0x02][0x00][0x00]
+        returns: bytes
+        """
         return bytes([
             DownlinkEncoder.CMD_REQUEST_STATUS,
             0x00, #blank 
@@ -42,6 +69,15 @@ class DownlinkEncoder:
     
     @staticmethod
     def encode_reset() -> bytes: 
+        """
+        encode device reset command
+        
+        payload:
+        [0x22][0x00][0x00]
+        
+        returns: 
+        bytes
+        """
         return bytes([
             DownlinkEncoder.CMD_RESET,
             0x00, #blank
@@ -51,6 +87,15 @@ class DownlinkEncoder:
     
     @staticmethod 
     def encode_get_region() -> bytes: 
+        """
+        encode request for current lora region
+
+        payload:
+        [0x20][0x00][0x00]
+
+        returns:
+        bytes
+        """
         return bytes([
             DownlinkEncoder.CMD_GET_REGION,
             0x00, #blank
@@ -59,6 +104,17 @@ class DownlinkEncoder:
 
     @staticmethod 
     def encode_set_region(region: int) -> bytes:
+        """
+        encode region update command
+        payload:
+        [0x21][0x00][0x01][region_id]
+
+        args:
+        region (int): region id (mapped from REGION_MAP)
+
+        returns:
+        bytes
+        """
         return bytes([
             DownlinkEncoder.CMD_SET_REGION,
             0x00,   # target
